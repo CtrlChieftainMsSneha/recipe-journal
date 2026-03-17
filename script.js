b24043a98f157c2521a30b6bdf984f7f127e9dc5
W <Sneha.M.Wilson-1@ou.edu>
2026-03-17 15:24:30 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+function addRecipe() {
 
+  let title = document.getElementById("title").value;
+  let ingredients = document.getElementById("ingredients").value;
+  let instructions = document.getElementById("instructions").value;
+
+  let recipe = `
+    <h3>${title}</h3>
+    <p><b>Ingredients:</b> ${ingredients}</p>
+    <p><b>Instructions:</b> ${instructions}</p>
+    <hr>
+  `;
+
+  document.getElementById("recipes").innerHTML += recipe;
+
+}
